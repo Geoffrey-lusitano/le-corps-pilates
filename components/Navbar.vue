7afa1d52023ce37bb353be97b4e0a4fb2a660099
geoffrey lusitano <g.bl@outlook.fr>
2022-06-21 17:24:54 +0200

    add fontawesome

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li>accueil</li>
+            <font-awesome-icon icon="fa-solid fa-1" />
+            <font-awesome-icon icon="fa-brands fa-facebook-f" />
+            <font-awesome-icon :icon="['fab', 'facebook-f']" />
+
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+
+<style>
+
+</style>
